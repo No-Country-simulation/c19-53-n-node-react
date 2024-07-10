bcaad6b4d2925fa76252cda0b756074b33ab2320
negrura14 <talleresmatador114@gmail.com>
2024-07-10 19:45:52 -0300

    se agrego la carpte img

--- FrontEnd/src/components/NavHome/NavHome.jsx
+++ FrontEnd/src/components/NavHome/NavHome.jsx
@@ -1,6 +1,7 @@
 import './NavHome.css';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../Helpers/RoutesPath';
+import logo from '../../img/Logo x1.png';
 
 
 function NavHome() {
@@ -10,6 +11,7 @@ function NavHome() {
             <nav>
                 <div className='nav_container'>
                     <div>
+                        <img src={logo} alt="logo" />
                         <Link to={ROUTES.LOGIN}><button>Iniciar Sesión</button></Link>
                         <Link to={ROUTES.CREATEACCOUNT}><button>Crear cuenta</button></Link>
                     </div>
