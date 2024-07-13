7b61da59659a6e2f700ce5c05084930f17e00330
RudeusGrayrat <miguelnc2502@gmail.com>
2024-07-12 20:10:39 -0500

    register, login, getUser

--- BackEnd/src/routes/usersRoutes.js
+++ BackEnd/src/routes/usersRoutes.js
@@ -1,5 +1,13 @@
 const { Router } = require('express');
-const getUsers = require('../controllers/Users/getUsers');
+
+const getUser = require('../controllers/Users/getUser');
+const postUsers = require('../controllers/Users/postUser');
+const login = require('../controllers/Users/login');
+
 const router = Router();
-router.get("/users", getUsers);
+
+router.get("/users", getUser);
+router.get("/login", login);
+router.post("/users", postUsers);
+
 module.exports = router;
