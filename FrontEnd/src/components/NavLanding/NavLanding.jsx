66cb7f534649e2f816d642a3936958e80048ee28
negrura14 <talleresmatador114@gmail.com>
2024-07-12 23:25:15 -0300

    Agregando estilos al landing

--- FrontEnd/src/components/NavLanding/NavLanding.jsx
+++ FrontEnd/src/components/NavLanding/NavLanding.jsx
@@ -1,6 +1,7 @@
 import "./NavLanding.css";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../Helpers/RoutesPath";
+import ConfirmationButton from '../../components/InputComponents/ConfirmationButton'
 // import logo from "../../Img/Logox1.png";
 
 function NavHome() {
@@ -8,27 +9,25 @@ function NavHome() {
     <header>
       <nav>
         <div>
-          <div className="nav_container">
+          <div className="nav_container bg-violet-300">
             <a
               href="/"
               className=" mb-2 col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
             >
               {/* <img src={logo} width="200px" alt="logo" /> */}
             </a>
-            <ul className="nav col-md-4 justify-content-end mb-2">
+            <div className="flex space-x-20 container flex justify-end items-center w-full">
               <Link
-                className="nav-link px-2 text-body-primary"
                 to={ROUTES.LOGIN}
               >
-                <button className="btn-nav-home">Iniciar Sesión</button>
+                <ConfirmationButton name={'Iniciar Sesión'} />
               </Link>
               <Link
-                className="nav-link px-2 text-body-primary"
                 to={ROUTES.CREATEACCOUNT}
               >
-                <button className="btn-nav-home">Crear cuenta</button>
+                <ConfirmationButton name={'Crear cuenta'} />
               </Link>
-            </ul>
+            </div>
           </div>
         </div>
       </nav>
