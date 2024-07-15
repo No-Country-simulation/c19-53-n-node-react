f35b4a01b77bf76f1809c408c7697328d9e5bae4
Alvaro Deguer <alvarodeguer@gmail.com>
2024-07-14 23:25:14 -0300

    Ordenados archivos index en uno solo

--- BackEnd/index.js
+++
@@ -1,9 +0,0 @@
-const server = require("./src/index");
-const connectDB = require("./src/db/dbConnection")
-const {
-    PORT
-} = process.env;
-connectDB()
-server.listen(PORT, "0.0.0.0", () => {
-    console.log(`${PORT}`);
-});
