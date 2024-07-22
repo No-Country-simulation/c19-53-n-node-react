41b951b532fdf160f80e9cf2e36569c8bee59a73
negrura14 <talleresmatador114@gmail.com>
2024-07-22 12:37:48 -0300

    se agrego el botton logout

--- FrontEnd/src/components/MenuHome/MenuHome.jsx
+++ FrontEnd/src/components/MenuHome/MenuHome.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import {useLocation, Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 import { SlHome } from "react-icons/sl"
 import { LuWallet } from "react-icons/lu";
@@ -10,20 +10,22 @@ import { FaMoneyBillTransfer } from "react-icons/fa6";
 import { FiUser } from "react-icons/fi";
 import { IoIosArrowForward } from "react-icons/io";
 import { FaListCheck } from "react-icons/fa6";
+import { FiLogOut } from "react-icons/fi";
+import { LuLogOut } from "react-icons/lu";
 
 function MenuHome() {
     const location = useLocation();
-    const [ active, setActive] = useState(location.pathname);
+    const [active, setActive] = useState(location.pathname);
 
     const handleActive = (path) => {
         setActive(path)
     }
 
-    return(
-        
+    return (
+
         <nav className="bg-violet-500 h-screen w-52">
-         {/* Botón de Usuario */}
-         <div className="flex justify-center items-center py-4">
+            {/* Botón de Usuario */}
+            <div className="flex justify-center items-center py-4">
                 <button className="flex items-center px-4 py-2 hover:bg-indigo-800 bg-indigo-900 text-white rounded-lg">
                     <FiUser className="mr-2" />
                     Usuario
@@ -32,47 +34,54 @@ function MenuHome() {
             </div>
             {/* Contenido del menú */}
             <div className="flex justify-center mt-0">
-                
-            <ul className="mt-5">
-                <Link to='/home ' className={`flex items-center block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/home' && 'bg-indigo-700'}`}
-                onClick={() => handleActive('/home')}>
-                <SlHome className="mr-5" /> 
-                    <span>Inicio</span>    
-                </Link>
-                <Link to='/banking' className={`flex items-center mt-5 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/banking' && 'bg-indigo-700'}`}
-                onClick={() => handleActive('/banking')}
-                >
-                <LuWallet className="mr-5"/>
-                    <span>Tu dinero</span>
+
+                <ul className="mt-5">
+                    <Link to='/home ' className={`flex items-center block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/home' && 'bg-indigo-700'}`}
+                        onClick={() => handleActive('/home')}>
+                        <SlHome className="mr-5 h-5 w-5" />
+                        <span>Inicio</span>
                     </Link>
-                <Link to='/activities' className={`flex items-center mt-5 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/activities' && 'bg-indigo-700'}`}
-                onClick={() => handleActive('/activities')}
-                >
-                <RxActivityLog className="mr-5"/>
-                    <span>Tu actividad</span>
+                    <Link to='/banking' className={`flex items-center mt-5 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/banking' && 'bg-indigo-700'}`}
+                        onClick={() => handleActive('/banking')}
+                    >
+                        <LuWallet className="mr-5 h-5 w-5" />
+                        <span>Tu dinero</span>
                     </Link>
-                <Link to='/pay' className={`flex items-center mt-5 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/pay' && 'bg-indigo-700'}`}
-                onClick={() => handleActive('/pay')}
-                >
-                <MdOutlineAttachMoney className="mr-5"/>
-                    <span>Pagar</span>
+                    <Link to='/activities' className={`flex items-center mt-5 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/activities' && 'bg-indigo-700'}`}
+                        onClick={() => handleActive('/activities')}
+                    >
+                        <RxActivityLog className="mr-5 h-5 w-5" />
+                        <span>Tu actividad</span>
                     </Link>
-                <Link to='/withdraw' className={`flex items-center mt-5 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/withdraw' && 'bg-indigo-700'}`}
-                onClick={() => handleActive('/withdraw')}
-                >
-                <FaMoneyBillTransfer className="mr-5"/>
-                    <span>Transferir dinero</span>
+                    <Link to='/pay' className={`flex items-center mt-5 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/pay' && 'bg-indigo-700'}`}
+                        onClick={() => handleActive('/pay')}
+                    >
+                        <MdOutlineAttachMoney className="mr-5 h-6 w-6" />
+                        <span>Pagar</span>
+                    </Link>
+                    <Link to='/withdraw' className={`flex items-center mt-5 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/withdraw' && 'bg-indigo-700'}`}
+                        onClick={() => handleActive('/withdraw')}
+                    >
+                        <FaMoneyBillTransfer className="mr-5 h-5 w-5" />
+                        <span>Transferir dinero</span>
                     </Link>
                     <Link to='/employees' className={`flex items-center mt-5 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/employees' && 'bg-indigo-700'}`}
-                onClick={() => handleActive('/employees')}
-                >
-                <FaListCheck className="mr-5"/>
-                    <span>Lista de empleados</span>
+                        onClick={() => handleActive('/employees')}
+                    >
+                        <FaListCheck className="mr-5 h-5 w-5" />
+                        <span>Lista de empleados</span>
                     </Link>
-            </ul>
+                    <div className="border-t border-white my-4 mx-4"></div>
+                    <Link to='/' className={`flex items-center mt-4 block py-2 px-4 text-white hover:bg-indigo-500 ${active === '/' && 'bg-indigo-700'}`}
+                        onClick={() => handleActive('/')}
+                    >
+                        <LuLogOut  className="mr-5 h-6 w-6" />
+                        <span>Cerrar sesión</span>
+                    </Link>
+                </ul>
             </div>
         </nav>
-        
+
     )
 }
 
