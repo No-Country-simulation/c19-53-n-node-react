65f68f5962e669f2bb9c408cf012a6fd110648be
Alvaro D. <alvarodeguer@gmail.com>
2024-07-28 20:23:55 -0300

    Agregando rutas para compañia

--- BackEnd/src/routes/companyRoutes.js
+++ BackEnd/src/routes/companyRoutes.js
@@ -4,12 +4,14 @@ const createCompany = require("../controllers/Company/createCompany");
 const createUser = require("../controllers/Company/createUser");
 const getAllCompanies = require("../controllers/Company/getAllCompanies");
 const deleteCompany = require("../controllers/Company/deleteCompany");
+const getEmployeesByCompany = require("../controllers/Company/getEmployeesByCompany");
 
 const router = Router();
 
 router.post("/companies", createCompany);
-router.post("/users", createUser);
+router.post("/companies/:companyId/users", createUser);
 router.delete("/company/:id", deleteCompany);
 router.get("/companies", getAllCompanies);
+router.get("/usersbycompany/:companyId", getEmployeesByCompany);
 
 module.exports = router;
