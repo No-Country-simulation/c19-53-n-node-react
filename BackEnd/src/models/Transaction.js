887d49d2303fd622292f73f8e9a6468c1b2b01c3
Alvaro D. <alvarodeguer@gmail.com>
2024-07-28 20:27:54 -0300

    Ordenando modelo de transacciones

--- BackEnd/src/models/Transaction.js
+++ BackEnd/src/models/Transaction.js
@@ -1,9 +1,31 @@
-// const transactionSchema = new mongoose.Schema({
-//     tipo: { type: String, enum: ['depósito', 'retiro', 'transferencia'] },
-//     monto: { type: Number, required: true },
-//     fecha: { type: Date, default: Date.now },
-//     usuarioEmisor: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' },
-//     usuarioReceptor: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' },
-// });
+const mongoose = require('mongoose');
 
-// const Transaccion = mongoose.model('Transaction', transactionSchema);
+const transactionSchema = new mongoose.Schema({
+    tipo: { 
+        type: String, 
+        enum: ['depósito', 'retiro', 'transferencia'], 
+        required: true 
+    },
+    monto: { 
+        type: Number, 
+        required: true 
+    },
+    fecha: { 
+        type: Date, 
+        default: Date.now 
+    },
+    usuarioEmisor: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Company',
+        required: true 
+    },
+    usuarioReceptor: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true
+    }
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction;
