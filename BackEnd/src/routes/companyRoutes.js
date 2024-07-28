d10134a945b1e01d24226a9b885b3083efc738cf
Alvaro D. <alvarodeguer@gmail.com>
2024-07-28 19:31:39 -0300

    Agredas rutas para compañia

--- BackEnd/src/routes/companyRoutes.js
+++ BackEnd/src/routes/companyRoutes.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 
 const createCompany = require("../controllers/Company/createCompany");
+const getAllCompanies = require("../controllers/Company/getAllCompanies");
+const deleteCompany = require("../controllers/Company/deleteCompany");
 
 const router = Router();
 
 router.post("/companies", createCompany);
+router.delete("/company/:id", deleteCompany);
+router.get("/companies", getAllCompanies);
 
 module.exports = router;
