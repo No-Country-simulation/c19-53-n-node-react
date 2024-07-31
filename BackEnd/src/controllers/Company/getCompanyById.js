ab8bb4095f3f31eeed1662c360a36069beb087d4
Alvaro Deguer <alvarodeguer@gmail.com>
2024-07-30 23:21:42 -0300

    Modificado metodos y rutas de compañia para mas escalabilidad

--- BackEnd/src/controllers/Company/getCompanyById.js
+++ BackEnd/src/controllers/Company/getCompanyById.js
@@ -1,15 +1,22 @@
-const CompanyModel = require('../../models/Company');
+const CompanyModel = require("../../models/Company");
 
 const getCompanyById = async (req, res) => {
-    try {
-        const company = await CompanyModel.findById(req.params.id);
-        if (!company) {
-            return res.status(404).json({ message: 'Company not found' });
-        }
-        res.status(200).json(company);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  const { id } = req.params;
+
+  try {
+    const company = await CompanyModel.findById(id);
+
+    if (!company) {
+      return res.status(404).json({ error: "Compañia no encontrada" });
     }
+
+    return res.status(200).json(company);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Error al recuperar compañia", details: error.message });
+  }
 };
 
 module.exports = getCompanyById;
