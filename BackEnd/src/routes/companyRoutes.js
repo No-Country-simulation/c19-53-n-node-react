16bf41a8593eb9ccb9e3fb9d436536c81fad2628
Alvaro Deguer <alvarodeguer@gmail.com>
2024-07-31 22:10:45 -0300

    Agregadas rutas para la gestion de transacciones

--- BackEnd/src/routes/companyRoutes.js
+++ BackEnd/src/routes/companyRoutes.js
@@ -2,22 +2,26 @@ const { Router } = require("express");
 
 const createCompany = require("../controllers/Company/createCompany");
 const createUser = require("../controllers/Company/createUser");
+const createTransaction = require("../controllers/Company/createTransaction.js");
 const getAllCompanies = require("../controllers/Company/getAllCompanies");
 const getCompanyById = require("../controllers/Company/getCompanyById");
+const getEmployeesByCompany = require("../controllers/Company/getEmployeesByCompany");
+const getTransactionByEmployeeId = require("../controllers/Company/getTransactionByEmployeeId");
 const deleteCompany = require("../controllers/Company/deleteCompany");
 const updateCompany = require("../controllers/Company/updateCompany");
-const getEmployeesByCompany = require("../controllers/Company/getEmployeesByCompany");
 const companylogin = require("../controllers/Company/loginCompany");
 
 const router = Router();
 
 router.post("/companies", createCompany);
 router.post("/companies/:companyId/users", createUser);
-router.delete("/company/:id", deleteCompany);
-router.put("/company/:id", updateCompany);
+router.post("/transaction/:emisorId/:receptorId", createTransaction);
+router.post("/company/login",companylogin);
 router.get("/companies", getAllCompanies);
 router.get("/companybyid/:id", getCompanyById);
 router.get("/usersbycompany/:companyId", getEmployeesByCompany);
-router.post("/company/login",companylogin);
+router.get("/transaction/:employeeId", getTransactionByEmployeeId);
+router.put("/company/:id", updateCompany);
+router.delete("/company/:id", deleteCompany);
 
 module.exports = router;
