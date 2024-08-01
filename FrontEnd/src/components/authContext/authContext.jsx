72a2e982e3c8aef8a4def530d6a09fea23e129dc
negrura14 <talleresmatador114@gmail.com>
2024-07-31 21:29:04 -0300

    se modifico el formulario de creacion de cuenta

--- FrontEnd/src/components/authContext/authContext.jsx
+++ FrontEnd/src/components/authContext/authContext.jsx
@@ -1,37 +1,37 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+// import React, { createContext, useContext, useState, useEffect } from 'react';
 
-const AuthContext = createContext();
+// const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+// export const AuthProvider = ({ children }) => {
+//     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    useEffect(()=>{
-        const getCookie = (name) =>{
-            const value = `; ${document.cookie}`;
-            const parts = value.split(`; ${name}=`);
-            if (parts.length === 2) return parts.pop().split(';').shift();
-        }
+//     useEffect(()=>{
+//         const getCookie = (name) =>{
+//             const value = `; ${document.cookie}`;
+//             const parts = value.split(`; ${name}=`);
+//             if (parts.length === 2) return parts.pop().split(';').shift();
+//         }
 
-        const cookieData = getCookie('miCookie');
-        console.log('Cookie data:', cookieData); 
+//         const cookieData = getCookie('miCookie');
+//         console.log('Cookie data:', cookieData); 
         
-        if (cookieData) {
-            try {
-                const userData = JSON.parse(decodeURIComponent(cookieData))
-                console.log('USER DATA', userData);
-                setIsAuthenticated(userData.auth);
+//         if (cookieData) {
+//             try {
+//                 const userData = JSON.parse(decodeURIComponent(cookieData))
+//                 console.log('USER DATA', userData);
+//                 setIsAuthenticated(userData.auth);
                 
-            } catch (error) {
-                console.error('Error parsing cookie data:', error);
-            }
-        }
-    }, [])
+//             } catch (error) {
+//                 console.error('Error parsing cookie data:', error);
+//             }
+//         }
+//     }, [])
 
-    return(
-        <AuthContext.Provider value={{isAuthenticated, setIsAuthenticated}}>
-            {children}
-        </AuthContext.Provider>
-    )
-};
+//     return(
+//         <AuthContext.Provider value={{isAuthenticated, setIsAuthenticated}}>
+//             {children}
+//         </AuthContext.Provider>
+//     )
+// };
 
-export const useAuth = () => useContext(AuthContext)
+// export const useAuth = () => useContext(AuthContext)
--- FrontEnd/src/context/testContext.jsx
+++ FrontEnd/src/context/testContext.jsx
@@ -3,9 +3,12 @@ import axios from "axios";
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 
-export const TestContext = createContext();
+export const TestContext = createContext({
+  company: {name: ''},
+  logout: () => {},
+});
 
-export const registerRequest = (user) => axios.post(`api/users`, user);
+export const registerRequest = (company) => axios.post(`api/companies`, company);
 export const getUsers = (user) => axios.get(`api/users`, user);
 export const deleteUser = (id) => axios.delete(`api/user/${id}`);
 export const getUserById = (id) => axios.get(`api/userbyid/${id}`);
@@ -16,6 +19,7 @@ export const companyLogin = (companyData) => axios.post('api/company/login', com
 export const TestProvider = ({ children }) => {
   const [company, setCompany] = useState(null);
   const [ loading, setLoading] = useState(true);
+  const  [isAuthenticated, setIsAuthenticated] = useState(false)
 
   const login = async (companyData) => {
     try {
@@ -23,6 +27,7 @@ export const TestProvider = ({ children }) => {
       setCompany(response.data.company)
       localStorage.setItem('company', JSON.stringify(response.data.company))
       Swal.fire('Success', 'Logged in successfully!', 'success')
+      setIsAuthenticated(true)
       return true;
     } catch (error) {
       Swal.fire('Error', 'Login failed!', 'error')
@@ -34,20 +39,24 @@ export const TestProvider = ({ children }) => {
     setCompany(null);
     localStorage.removeItem('compány');
     Swal.fire('Success', 'Logged out successfully!', 'success');
+    setIsAuthenticated(false)
+    setTimeout(() => {
       navigate('/')
+    }, 2000)
   }
 
   useEffect(() => {
     const storedCompany = localStorage.getItem('company');
     if(storedCompany) {
       setCompany(JSON.parse(storedCompany));
+      setIsAuthenticated(true)
     }
     setLoading(false)
   }, []);
 
   return (
     <TestContext.Provider
-      value={{ company, loading, login, logout, getUsers, registerRequest, deleteUser, getUserById, updateUser }}
+      value={{ isAuthenticated, company, loading, login, logout, getUsers, registerRequest, deleteUser, getUserById, updateUser }}
     >
       {children}
     </TestContext.Provider>
